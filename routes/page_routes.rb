dbd9a76dfaf27f1f590994bedab5fd9cc42d1af8
Tad Thorley <tadt@decho.com>
2009-11-08 20:15:48 -0700

    yeah, it's kinda screwy, but it reads more like english

--- routes/page_routes.rb
+++ routes/page_routes.rb
@@ -1,7 +1,7 @@
-get 'page/read/:page_number' do
+get '/read/page/:page_number' do
   haml :'page/read'
 end
 
-get '/page/edit/:page_number' do
+get '/edit/page/:page_number' do
   haml :'page/edit'
 end
