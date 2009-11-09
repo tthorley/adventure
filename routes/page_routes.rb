152dcaf6f5c778aadea573d3bfae2d4f9bb36d06
Tad Thorley <tadt@decho.com>
2009-11-09 00:31:04 -0700

    tweak page routes, page fleshed out a bit more

--- routes/page_routes.rb
+++ routes/page_routes.rb
@@ -1,14 +1,18 @@
 get '/read/page/:page_number' do
   @page = Page.get(params[:page_number])
   if @page
-    @footer_left = @page.author.username
+    @footer_left = @page.author.name
     @footer_right = @page.id
     haml :'page/read'
   else
-    haml :'page/missing'
+    redirect '/missing/page'
   end
 end
 
 get '/edit/page/:page_number' do
   haml :'page/edit'
+end
+
+get '/missing/page' do
+  haml :'page/missing'
 end
