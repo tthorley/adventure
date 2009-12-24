da586510e85e560be77d08e6f34468fe28218faa
Tad Thorley <tadt@decho.com>
2009-12-23 22:54:37 -0700

    This turned out more complex than I thought.  I'm going to redo it in rails.

--- models/page_model.rb
+++ models/page_model.rb
@@ -7,7 +7,7 @@ class Page
   property :created_at,  DateTime
   property :modified_at, DateTime
   property :choice_id,   Integer
-  property :author_id,     Integer
+  property :author_id,   Integer
   
   has n, :choices
   belongs_to :author
